e5c160fa7fe35a8feea6d670f4bfa80be581d2ed
Yannick Gibson <yannick.gibson@seznam.cz>
2021-03-14 15:45:11 +0100

    initial init

---
+++ XOR Sifrovani/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace XOR_Sifrovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valid file names ... .txt, .cry . | e. g. test.cry");
+            while (true)
+            {
+                Console.Write("Enter file name: ");
+                try
+                {
+                    Crypto("PRAVDA", Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("-Error: " + e.Message);
+                }
+            }
+        }
+        static void Crypto(string key, string fileName)
+        {
+            string oldFile = File.ReadAllText(fileName);
+
+            StringBuilder newFileContents = new StringBuilder();
+            for (int i = 0; i < oldFile.Length; i++)
+            {
+                newFileContents.Append((char)(key[i % 5] ^ oldFile[i]));
+            }
+            
+            string newFileName = fileName.Substring(0,fileName.IndexOf('.'));
+
+            string ext = fileName.Substring(fileName.IndexOf('.'));
+            if (ext == ".txt")
+                newFileName += ".cry";
+            else if (ext == ".cry")
+                newFileName += ".txt";
+            else
+                throw new Exception("Unsupported file extension");
+
+            File.Create(newFileName).Dispose();
+            File.AppendAllText(newFileName, newFileContents.ToString());
+            Console.WriteLine($"Result: \"{newFileContents}\"");
+            Console.WriteLine($"- Text was saved in the file '{newFileName}'");
+        }
+    }
+}
